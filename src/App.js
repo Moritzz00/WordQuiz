2b0a93b8eb732d5652dfedbf3897558e751c9d1c
Moritzz00 <moritz.zitzow@gmail.com>
2025-07-27 18:08:05 +0200

    add simple success screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { Children, useState } from 'react';
 import Keyboard from './Keyboard';
 import WordGuess from './Wordguess';
-import { maxGuessLength,attempts } from './Constants';
+import { maxGuessLength, attempts, SECRETWORD } from './Constants';
 
 export default function Game() {
   const [turn, setTurn] = useState(0);
-
   const [guesses, setGuesses] = useState(Array(attempts).fill(""))
   const currentGuess = guesses[turn]
+  const guessedCorrectly = currentGuess === SECRETWORD
 
   const handleKeyClick = (letter) => {
-    if (currentGuess.length <= (maxGuessLength-1) ) {
+    if (currentGuess.length <= (maxGuessLength - 1)) {
 
       let newGuess = currentGuess + letter
       let updatedGuesses = [...guesses]
@@ -34,27 +34,44 @@ export default function Game() {
   const handleSubmit = () => {
     if (currentGuess.length < maxGuessLength) { return };
 
-    if (turn <= (attempts-1)) {
+    if (turn <= (attempts - 1)) {
       setTurn(turn + 1)
     };
   }
 
-  return (
-    <div>
-      <WordGuess modalOpener={open} id={0} turn={turn} guess={guesses[0]} />
-      <WordGuess modalOpener={open} id={1} turn={turn} guess={guesses[1]} />
-      <WordGuess modalOpener={open} id={2} turn={turn} guess={guesses[2]} />
-      <WordGuess modalOpener={open} id={3} turn={turn} guess={guesses[3]} />
-      <WordGuess modalOpener={open} id={4} turn={turn} guess={guesses[4]} />
-      <WordGuess modalOpener={open} id={5} turn={turn} guess={guesses[5]} />
-      <Keyboard
-        handleKeyClick={handleKeyClick}
-        handleBackspace={handleBackspace}
-        handleSubmit={handleSubmit}
-      />
-    </div>
-  )
+  if (!guessedCorrectly) {
+    return (
+      <div>
+        <WordGuess modalOpener={open} id={0} turn={turn} guess={guesses[0]} />
+        <WordGuess modalOpener={open} id={1} turn={turn} guess={guesses[1]} />
+        <WordGuess modalOpener={open} id={2} turn={turn} guess={guesses[2]} />
+        <WordGuess modalOpener={open} id={3} turn={turn} guess={guesses[3]} />
+        <WordGuess modalOpener={open} id={4} turn={turn} guess={guesses[4]} />
+        <WordGuess modalOpener={open} id={5} turn={turn} guess={guesses[5]} />
+        <Keyboard
+          handleKeyClick={handleKeyClick}
+          handleBackspace={handleBackspace}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <>
+        You guessed correct!
+        <button
+          onClick={function () {
+            setTurn(0)
+            setGuesses(Array(attempts).fill(""))
+            return
+          }
+          }
+        >
+          Restart Game
+        </button>
+      </>
+    )
+  }
 }
-
 
 
