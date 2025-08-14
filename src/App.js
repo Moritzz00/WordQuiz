d3934395db396e438f423132482bb241639d0c7c
Moritzz00 <moritz.zitzow@gmail.com>
2025-08-14 18:35:24 +0200

    local routing works with / as home and /Game as the Game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Game from "./Game";
+import Home from "./Home";
+import { Route, Routes } from 'react-router';
 
 export default function App() {
   return (
-    <Game/>
+    <Routes>
+      <Route exact path="/"  element={<Home />} />
+      <Route path="/Game"  element={<Game />} />
+    </Routes>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
 import App from "./App";
+import { BrowserRouter } from "react-router";
 
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <BrowserRouter basename="/WordQuiz">
       <App />
+    </BrowserRouter>
   </StrictMode>
 );
