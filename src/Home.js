729e6624190545578d493d6c7800d0c42272c6a5
Moritzz00 <moritz.zitzow@gmail.com>
2025-08-16 16:25:10 +0200

    Added Birthday variant for the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,40 @@
 import { NavLink } from "react-router";
+import dayjs from "dayjs";
 
 export default function Home() {
+  const today = dayjs()
+  const birthdayToday = today.year(2025).month(7).date(21).format("YYYY-MM-DD") === today.format("YYYY-MM-DD")
+  return (
+    <div>
+      {
+      birthdayToday ?
+      <BirthdayHome/>
+      :
+      <StandardHome/>
+    }
+    </div>
+  )
+}
+
+function StandardHome() {
   return (
     <div>
       <h1>
-        This is the home Site
+        This is the standard home Site
       </h1>
       <nav>
         <NavLink to="/Game">
           To Game
         </NavLink>
       </nav>
+    </div>
+  )
+}
+
+function BirthdayHome() {
+  return (
+    <div>
+
     </div>
   )
 }
